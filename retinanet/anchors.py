da8168e4887db2efc03d9e371f99a0a2351bc84a
juliamgomes <julia.mgomes170@gmail.com>
2020-05-02 23:21:08 +0100

    make anchors match WayveCode'

--- retinanet/anchors.py
+++ retinanet/anchors.py
@@ -7,14 +7,11 @@ class Anchors(nn.Module):
     def __init__(self, pyramid_levels=None, strides=None, sizes=None, ratios=None, scales=None):
         super(Anchors, self).__init__()
 
-        if pyramid_levels is None:
-            self.pyramid_levels = [3, 4, 5, 6, 7]
-        if strides is None:
-            self.strides = [2 ** x for x in self.pyramid_levels]
+        self.pyramid_levels = [3, 4, 5]
+        self.strides = [2 ** x for x in self.pyramid_levels]
         self.sizes = [2 ** x for x in self.pyramid_levels]
         self.ratios = np.array([2, 2.5, 3])
-        if scales is None:
-            self.scales = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)])
+        self.scales = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)])
 
     def forward(self, image):
         
